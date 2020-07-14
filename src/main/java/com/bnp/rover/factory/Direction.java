c820db9c44d973128b7cc4adaa4bed8630472dd9
ablaghj <blaghji.alaeddine@gmail.com>
2020-07-14 16:02:07 +0200

    [fix] name param

--- src/main/java/com/bnp/rover/factory/Direction.java
+++ src/main/java/com/bnp/rover/factory/Direction.java
@@ -3,5 +3,5 @@ package com.bnp.rover.factory;
 import com.bnp.rover.enums.DirectionType;
 
 public abstract class Direction {
-    public abstract DirectionType nextDirection(DirectionType CurrentDirection);
+    public abstract DirectionType nextDirection(DirectionType currentDirection);
 }
